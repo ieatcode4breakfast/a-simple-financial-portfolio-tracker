40741dffa9a8125b29d871bea6284f184c2c2c45
iEatCode4Breakfast <dwayneletran17@gmail.com>
2024-06-27 04:32:14 +0800

    Add mockFetch and test inputs

---
+++ scripts/tests/mockFetch.js
@@ -0,0 +1,43 @@
+// Simulate an async fetch operation
+
+async function mockFetch(ticker) {
+  return mockLastPriceData.find(asset => asset.ticker === ticker);
+}
+
+const mockAssetData = [
+  { ticker: 'BTC-USD', name: 'Bitcoin', lastPrice: 61006.35 },
+  { ticker: 'ALGO-USD', name: 'Algorand', lastPrice: 0.139765 },
+  { ticker: 'GOOG', name: 'Alphabet', lastPrice: 133.27 },
+  { ticker: 'AAPL', name: 'Apple', lastPrice: 213.96 },
+  { ticker: 'PLTR', name: 'Palantir Technologies', lastPrice: 24.23 },
+  { ticker: 'SOFI', name: 'SoFi Technologies', lastPrice: 7.17 },
+  { ticker: 'AMD', name: 'Advanced Micro Devices', lastPrice: 157.07 },
+  { ticker: 'DNUT', name: 'Krispy Kreme', lastPrice: 15.89 },
+  { ticker: 'NVDA', name: 'NVIDIA Corporation', lastPrice: 124.02 },
+  { ticker: 'RIVN', name: 'Rivian Automotive', lastPrice: 14.53 },
+  { ticker: 'TSLA', name: 'Tesla', lastPrice: 195.63 },
+  { ticker: 'NIO', name: 'NIO', lastPrice: 4.585 },
+  { ticker: 'AMZN', name: 'Amazon', lastPrice: 194.63 },
+  { ticker: 'CCL', name: 'Carnival Corporation', lastPrice: 18.47 },
+  { ticker: 'SIRI', name: 'Sirius XM Holdings', lastPrice: 2.705 },
+  { ticker: 'MARA', name: 'Marathon Digital Holdings', lastPrice: 19.84 },
+  { ticker: 'MU', name: 'Micron Technology', lastPrice: 141.78 },
+  { ticker: 'BAC', name: 'Bank of America', lastPrice: 38.54 },
+  { ticker: 'AAL', name: 'American Airlines', lastPrice: 11.16 },
+  { ticker: 'F', name: 'Ford Motor Company', lastPrice: 12.02 },
+  { ticker: 'NU', name: 'Nu Holdings', lastPrice: 12.64 },
+  { ticker: 'LCID', name: 'Lucid Group', lastPrice: 2.62 },
+  { ticker: 'CMG', name: 'Chipotle Mexican Grill', lastPrice: 65.81 },
+  { ticker: 'VALE', name: 'Vale S.A.', lastPrice: 11.13 },
+  { ticker: 'PFE', name: 'Pfizer', lastPrice: 27.38 },
+  { ticker: 'CLSK', name: 'CleanSpark', lastPrice: 16.44 },
+  { ticker: 'CHWY', name: 'Chewy', lastPrice: 30.05 },
+  { ticker: 'ET', name: 'Energy Transfer', lastPrice: 15.93 },
+  { ticker: 'RIOT', name: 'Riot Platforms', lastPrice: 9.21 },
+  { ticker: 'T', name: 'AT&T', lastPrice: 18.7 },
+  { ticker: 'WFC', name: 'Wells Fargo', lastPrice: 57.14 },
+  { ticker: 'DIS', name: 'Disney', lastPrice: 102.15 }
+];
+
+
+export default mockFetch;
---
+++ scripts/tests/testInputs.js
@@ -0,0 +1,57 @@
+import {addPosition} from './positions.js';
+
+export const testInput1 = {
+  type: 'Crypto',
+  ticker: 'BTC',
+  shares: 0.010851,
+  totalCost: 732.33,
+};
+
+export const testInput2 = {
+  type: 'Crypto',
+  ticker: 'ALGO',
+  shares: 1324.488921,
+  totalCost: 265.56,
+};
+
+export const testInput3 = {
+  type: 'Stock',
+  ticker: 'AAPL',
+  shares: 0.643900,
+  totalCost: 110.35,
+};
+
+export const testInput4 = {
+  type: 'Stock',
+  ticker: 'PLTR',
+  shares: 4.363000,
+  totalCost: 100.35,
+};
+
+export const testInput5 = {
+  type: 'Stock',
+  ticker: 'SOFI',
+  shares: 13.989600,
+  totalCost: 100.31,
+};
+
+export const testInput6 = {
+  type: 'Stock',
+  ticker: 'AMD',
+  shares: 0.636900,
+  totalCost: 99.35,
+};
+
+export const testInput7 = {
+  type: 'Stock',
+  ticker: 'WFC',
+  shares: 1.705100,
+  totalCost: 100.29,
+};
+
+export const testInput8 = {
+  type: 'Stock',
+  ticker: 'DIS',
+  shares: 0.977400,
+  totalCost: 99.31,
+};
