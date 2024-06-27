a0463ed2eca6eff58374ad6409faf6bc5dadafca
iEatCode4Breakfast <dwayneletran17@gmail.com>
2024-06-28 02:40:44 +0800

    Rename portoflioTable.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import Asset from './data/asset.js';
 import Portfolio from './data/portfolio.js';
-import PortfolioTable from './components/portfolio-table.js';
+import PortfolioTable from './components/portfolioTable.js';
 
 export const portfolio = new Portfolio;
 export const portfolioTable = new PortfolioTable;
