78e2011b0865cfe7a25aa173593e57198359cc18
iEatCode4Breakfast <dwayneletran17@gmail.com>
2024-07-03 10:42:21 +0800

    Add popup window

---
+++ scripts/components/popup.js
@@ -0,0 +1,15 @@
+class Popup {
+  #headerText;
+  #actionText;
+
+  constructor(headerText, actionText) {
+    this.#headerText = headerText;
+    this.#actionText = actionText;
+  }
+
+  display() {
+    
+  }
+}
+
+export default Popup;
--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ import Portfolio from './data/portfolio.js';
 import PortfolioTable from './components/portfolioTable.js';
 import Summary from './components/summary.js';
 import marketData from './data/marketData.js';
+import Popup from './components/popup.js';
 import { submitTestInput } from './tests/testInputs.js';
 
 const portfolio = new Portfolio();
@@ -18,7 +19,7 @@ document.querySelector('.js-add-asset')
 
 document.querySelector('.js-edit-cash')
   .addEventListener('click', () => {
-    
+
   });
 
 document.querySelector('.js-reset-portfolio')
